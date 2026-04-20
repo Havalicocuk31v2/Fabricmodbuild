fb9dcda0658925a1ced91e4c20b93727d57ea5af
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-20 19:50:38 +0300

    Update AimAssist.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
@@ -6,42 +6,40 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 
 public class AimAssist extends Module {
     protected static final MinecraftClient mc = MinecraftClient.getInstance();
-    
-    public Setting speed = new Setting("Speed", 1.2, 0, 100);
-    public Setting smoothness = new Setting("Smoothness", 3.0, 1, 10);
-    public double range = 4.2;
+    public Setting speed = new Setting("Speed", 2.0, 0, 20); // 100 çok fazla, 20 idealdir
+    public Setting range = new Setting("Range", 4.2, 3.0, 6.0);
 
     public AimAssist() { 
         super("Aim Assist", -1);
         addSetting(speed);
-        addSetting(smoothness);
+        addSetting(range);
     }
 
     @Override
     public void onTick() {
-        if (mc.currentScreen != null || mc.player == null || mc.world == null) return;
+        if (!this.isEnabled() || mc.currentScreen != null || mc.player == null) return;
+        
         Entity target = getTarget();
         if (target != null) {
-            Vec3d targetPos = target.getPos().add(0, target.getStandingEyeHeight() - 0.15, 0);
-            Vec3d playerPos = mc.player.getEyePos();
+            double diffX = target.getX() - mc.player.getX();
+            double diffZ = target.getZ() - mc.player.getZ();
+            float targetYaw = (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0);
             
-            float targetYaw = (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(targetPos.z - playerPos.z, targetPos.x - playerPos.x)) - 90.0);
-            float targetPitch = (float) MathHelper.wrapDegrees(-Math.toDegrees(Math.atan2(targetPos.y - playerPos.y, Math.sqrt(Math.pow(targetPos.x - playerPos.x, 2) + Math.pow(targetPos.z - playerPos.z, 2)))));
-
-            // Polar Bypass Smooth Logic: Titremeyi engeller
-            float softSpeed = (float) (speed.getValue() / (smoothness.getValue() * 10));
-            mc.player.setYaw(mc.player.getYaw() + MathHelper.clamp(MathHelper.wrapDegrees(targetYaw - mc.player.getYaw()), -softSpeed, softSpeed));
-            mc.player.setPitch(mc.player.getPitch() + MathHelper.clamp(MathHelper.wrapDegrees(targetPitch - mc.player.getPitch()), -softSpeed, softSpeed));
+            float yawDiff = MathHelper.wrapDegrees(targetYaw - mc.player.getYaw());
+            float step = (float) speed.getValue();
+            
+            if (Math.abs(yawDiff) > 0.1) {
+                mc.player.setYaw(mc.player.getYaw() + MathHelper.clamp(yawDiff, -step, step));
+            }
         }
     }
 
     private Entity getTarget() {
         for (Entity e : mc.world.getEntities()) {
-            if (e instanceof PlayerEntity && e != mc.player && e.isAlive() && mc.player.distanceTo(e) <= range) return e;
+            if (e instanceof PlayerEntity && e != mc.player && e.isAlive() && mc.player.distanceTo(e) <= range.getValue()) return e;
         }
         return null;
     }
