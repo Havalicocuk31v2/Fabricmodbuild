83f164959365042f774a14996714ffba7b7b9bb6
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-20 19:39:22 +0300

    Update Module.java

--- src/main/java/dev/lvstrng/argon/module/Module.java
+++ src/main/java/dev/lvstrng/argon/module/Module.java
@@ -1,28 +1,25 @@
 package dev.lvstrng.argon.module;
-import net.minecraft.client.MinecraftClient;
 
-public abstract class Module {
-    protected static final MinecraftClient mc = MinecraftClient.getInstance();
+import java.util.ArrayList;
+import java.util.List;
+
+public class Module {
     private String name;
     private int key;
     private boolean enabled;
+    private List<Setting> settings = new ArrayList<>(); // Ayarları tutan liste
 
     public Module(String name, int key) {
         this.name = name;
         this.key = key;
-        this.enabled = false;
     }
 
-    public void toggle() {
-        this.enabled = !this.enabled;
-        if (enabled) onEnable(); else onDisable();
-    }
-
-    public void onTick() {}
-    public void onEnable() {}
-    public void onDisable() {}
+    public void addSetting(Setting s) { settings.add(s); }
+    public List<Setting> getSettings() { return settings; }
+    
     public String getName() { return name; }
-    public int getKey() { return key; }
-    public void setKey(int key) { this.key = key; }
     public boolean isEnabled() { return enabled; }
+    public void toggle() { this.enabled = !this.enabled; }
+    
+    public void onTick() {}
 }
