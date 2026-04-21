b1ca4f634c8ec47c3ddb93ca4e7bbd4d9af6c5f4
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-21 15:26:48 +0300

    Update Setting.java

--- src/main/java/dev/lvstrng/argon/module/Setting.java
+++ src/main/java/dev/lvstrng/argon/module/Setting.java
@@ -3,11 +3,11 @@ package dev.lvstrng.argon.module;
 public class Setting {
     public String name;
     public double value, min, max;
-    public boolean enabled; // On/Off ayarları için
+    public boolean enabled;
     public boolean isBoolean;
     public boolean dragging = false;
 
-    // Sayısal ayar (Slider) için
+    // Sayısal ayar (Slider)
     public Setting(String name, double defaultValue, double min, double max) {
         this.name = name;
         this.value = defaultValue;
@@ -16,12 +16,16 @@ public class Setting {
         this.isBoolean = false;
     }
 
-    // On/Off ayarı için
+    // On/Off ayarı
     public Setting(String name, boolean enabled) {
         this.name = name;
         this.enabled = enabled;
         this.isBoolean = true;
     }
+
+    // DERLEME HATASINI ÇÖZEN METODLAR
+    public double getValue() { return value; }
+    public boolean isEnabled() { return enabled; }
 
     public void setValue(double value) { 
         this.value = Math.round(Math.min(max, Math.max(min, value)) * 10.0) / 10.0; 
