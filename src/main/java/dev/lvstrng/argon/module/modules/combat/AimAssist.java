8b3a7ac421486f4ee67575804232382c79a5d4ad
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-21 14:59:35 +0300

    Update AimAssist.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
@@ -8,7 +8,7 @@ import net.minecraft.util.math.MathHelper;
 
 public class AimAssist extends Module {
     protected static final MinecraftClient mc = MinecraftClient.getInstance();
-    public Setting speed = new Setting("Speed", 5.0, 0, 100);
+    public Setting speed = new Setting("Speed", 10.0, 0, 100);
     public Setting range = new Setting("Range", 4.0, 3.0, 6.0);
 
     public AimAssist() { 
@@ -26,16 +26,13 @@ public class AimAssist extends Module {
             double diffX = target.getX() - mc.player.getX();
             double diffZ = target.getZ() - mc.player.getZ();
             float targetYaw = (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0);
-            
             float yawDiff = MathHelper.wrapDegrees(targetYaw - mc.player.getYaw());
             
-            // Polar Bypass: Hız yüksek olsa bile hareketi titreşimli (insansı) yapar
-            double randomNoise = (Math.random() - 0.5) * 0.2; 
-            float currentSpeed = (float) (speed.getValue() + randomNoise);
+            // Ultra hızlı ama rastgele gürültülü (Polar Bypass)
+            float currentSpeed = (float) (speed.getValue() + (Math.random() - 0.5));
             
             if (Math.abs(yawDiff) > 0.1) {
-                float move = MathHelper.clamp(yawDiff, -currentSpeed, currentSpeed);
-                mc.player.setYaw(mc.player.getYaw() + move);
+                mc.player.setYaw(mc.player.getYaw() + MathHelper.clamp(yawDiff, -currentSpeed, currentSpeed));
             }
         }
     }
