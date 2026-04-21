cccffd3bac811e15e7fb6259d35193c4a93d032f
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-21 15:54:31 +0300

    Update Module.java

--- src/main/java/dev/lvstrng/argon/module/Module.java
+++ src/main/java/dev/lvstrng/argon/module/Module.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Module {
     private String name;
     private int key;
-    private boolean enabled;
+    private boolean enabled = false;
     private List<Setting> settings = new ArrayList<>();
 
     public Module(String name, int key) {
@@ -18,7 +18,6 @@ public class Module {
     public List<Setting> getSettings() { return settings; }
     public String getName() { return name; }
     public boolean isEnabled() { return enabled; }
-    public void setEnabled(boolean enabled) { this.enabled = enabled; }
     public void toggle() { this.enabled = !this.enabled; }
     public int getKey() { return key; }
     public void setKey(int key) { this.key = key; }
