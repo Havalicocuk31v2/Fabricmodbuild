ee4e4d11137f142169a2f244204cb3787202f7c3
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-21 15:20:36 +0300

    Update AimAssist.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
@@ -8,13 +8,18 @@ import net.minecraft.util.math.MathHelper;
 
 public class AimAssist extends Module {
     protected static final MinecraftClient mc = MinecraftClient.getInstance();
-    public Setting speed = new Setting("Speed", 10.0, 0, 100);
-    public Setting range = new Setting("Range", 4.0, 3.0, 6.0);
+    
+    public Setting speed = new Setting("Speed", 5.0, 0, 100);
+    public Setting range = new Setting("Range", 4.2, 3.0, 6.0);
+    public Setting onHead = new Setting("On the Head", true);
+    public Setting onChest = new Setting("On the Chest", false);
 
     public AimAssist() { 
-        super("Aim Assist", -1);
+        super("Aim Assist", -1); // Varsayılan tuş yok
         addSetting(speed);
         addSetting(range);
+        addSetting(onHead);
+        addSetting(onChest);
     }
 
     @Override
@@ -23,16 +28,30 @@ public class AimAssist extends Module {
         
         PlayerEntity target = getTarget();
         if (target != null) {
+            // Hedef Noktası Hesaplama (Head vs Chest)
+            double targetY = target.getY();
+            if (onHead.enabled) targetY = target.getEyeY() - 0.1;
+            else if (onChest.enabled) targetY = target.getY() + (target.getHeight() / 1.5);
+
             double diffX = target.getX() - mc.player.getX();
             double diffZ = target.getZ() - mc.player.getZ();
+            double diffY = targetY - mc.player.getEyeY();
+            double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
             float targetYaw = (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0);
+            float targetPitch = (float) MathHelper.wrapDegrees(-Math.toDegrees(Math.atan2(diffY, diffXZ)));
+
+            // SIFIR TİTREME (Smooth Logic)
             float yawDiff = MathHelper.wrapDegrees(targetYaw - mc.player.getYaw());
-            
-            // Ultra hızlı ama rastgele gürültülü (Polar Bypass)
-            float currentSpeed = (float) (speed.getValue() + (Math.random() - 0.5));
-            
-            if (Math.abs(yawDiff) > 0.1) {
+            float pitchDiff = MathHelper.wrapDegrees(targetPitch - mc.player.getPitch());
+
+            float currentSpeed = (float) (speed.getValue() * 0.1); // Ultra hassas kontrol
+
+            if (Math.abs(yawDiff) > 0.05) {
                 mc.player.setYaw(mc.player.getYaw() + MathHelper.clamp(yawDiff, -currentSpeed, currentSpeed));
+            }
+            if (Math.abs(pitchDiff) > 0.05) {
+                mc.player.setPitch(mc.player.getPitch() + MathHelper.clamp(pitchDiff, -currentSpeed, currentSpeed));
             }
         }
     }
