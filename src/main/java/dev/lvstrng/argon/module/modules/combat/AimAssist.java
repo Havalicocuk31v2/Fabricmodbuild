2facdac139329df3d29d2ee6d5093a5146be9eb4
Havalicocuk31v2 <sevketcinar215@gmail.com>
2026-04-21 15:27:23 +0300

    Update AimAssist.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AimAssist.java
@@ -15,7 +15,7 @@ public class AimAssist extends Module {
     public Setting onChest = new Setting("On the Chest", false);
 
     public AimAssist() { 
-        super("Aim Assist", -1); // Varsayılan tuş yok
+        super("Aim Assist", -1);
         addSetting(speed);
         addSetting(range);
         addSetting(onHead);
@@ -28,11 +28,8 @@ public class AimAssist extends Module {
         
         PlayerEntity target = getTarget();
         if (target != null) {
-            // Hedef Noktası Hesaplama (Head vs Chest)
-            double targetY = target.getY();
-            if (onHead.enabled) targetY = target.getEyeY() - 0.1;
-            else if (onChest.enabled) targetY = target.getY() + (target.getHeight() / 1.5);
-
+            double targetY = onHead.isEnabled() ? target.getEyeY() - 0.1 : target.getY() + (target.getHeight() / 1.5);
+            
             double diffX = target.getX() - mc.player.getX();
             double diffZ = target.getZ() - mc.player.getZ();
             double diffY = targetY - mc.player.getEyeY();
@@ -41,11 +38,11 @@ public class AimAssist extends Module {
             float targetYaw = (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0);
             float targetPitch = (float) MathHelper.wrapDegrees(-Math.toDegrees(Math.atan2(diffY, diffXZ)));
 
-            // SIFIR TİTREME (Smooth Logic)
             float yawDiff = MathHelper.wrapDegrees(targetYaw - mc.player.getYaw());
             float pitchDiff = MathHelper.wrapDegrees(targetPitch - mc.player.getPitch());
 
-            float currentSpeed = (float) (speed.getValue() * 0.1); // Ultra hassas kontrol
+            // Titreme engelleyici yumuşak geçiş hızı
+            float currentSpeed = (float) (speed.getValue() * 0.15);
 
             if (Math.abs(yawDiff) > 0.05) {
                 mc.player.setYaw(mc.player.getYaw() + MathHelper.clamp(yawDiff, -currentSpeed, currentSpeed));
